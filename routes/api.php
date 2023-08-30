8287f43e42852412ce88e773da0f77a012163656
maicol ubaque <maikolubaque@gmail.com>
2023-08-30 11:57:28 -0500

    fix route type

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\TypeController;
 use Illuminate\Support\Facades\Route;
 
 # Todas las rutas para hotel
-Route::resource('hotels', HotelController::class)->except(['create', 'edit']);
+Route::resource('hotel', HotelController::class)->except(['create', 'edit']);
 Route::resource('room', RoomController::class)->except(['create', 'edit', 'index']);
 Route::get('hotel/{id}/rooms', [RoomController::class, 'roomsHotel']);
 Route::get('type', [TypeController::class, 'index']);
